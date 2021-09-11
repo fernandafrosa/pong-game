bc6031135c2fe429a85923ab8a0422ce86ef464f
Fernanda <fernanda.frosa@icloud.com>
2021-09-11 17:05:02 -0300

    paddle class created

--- main.py
+++ main.py
@@ -13,14 +13,33 @@
 
 #TODO: importing libraries
 from turtle import Screen
-import time
+from paddle import Paddle
 
 #TODO: create the screen
 screen = Screen()
-screen.setup(width=1000, height=800)
-screen.bgcolor('black')
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
 screen.title("Fernanda´s Pong Game")
 screen.tracer(0)
 
+
+
+#TODO: CREATE PADDLE:
+
+r_paddle = Paddle(350, 0)
+l_paddle = Paddle(-350, 0)
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+
+
+game_on = True
+while game_on:
+    screen.update()
 
 screen.exitonclick()

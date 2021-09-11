56f2f5bf6e839b4a85911a99c65d97f5d1026dfc
Fernanda <fernanda.frosa@icloud.com>
2021-09-11 17:58:23 -0300

    detection of misses

--- ball.py
+++ ball.py
@@ -23,3 +23,7 @@ class Ball(Turtle):
 
     def bounce_x(self):
         self.x_move *= -1 #reverse x direction
+
+    def reset_pos(self):
+        self.goto(0,0)
+        self.bounce_x()
--- main.py
+++ main.py
@@ -53,5 +53,13 @@ while game_on:
     if (ball.distance(r_paddle) < 50 and ball.xcor() > 320) or (ball.distance(l_paddle) < 50 and ball.xcor() < -320):
         ball.bounce_x()
 
+    #TODO: Detect when R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_pos()
+
+    #TODO: Detect when L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_pos()
+
 
 screen.exitonclick()
